009f7477c2668e3e3898948c61de73fab3f1de15
Bavana Venkata Sumanth <sumanthbv8@gmial.com>
2022-06-20 13:36:41 +0530

    final push first installment-deploy-14

--- frontend/axiosFetch.js
+++ frontend/axiosFetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-    process.env.NEXT_NODE_ENV === "production"
+    process.env.NEXT_PUBLIC_NODE_ENV === "production"
         ? `${process.env.NEXT_BACK_URL}`
         : "";
 
