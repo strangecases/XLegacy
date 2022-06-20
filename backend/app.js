1c24ca52768c76eab1fe8b61e06b1e1ec74c985a
Bavana Venkata Sumanth <sumanthbv8@gmial.com>
2022-06-20 22:51:14 +0530

    final-push-all-7

--- backend/app.js
+++ backend/app.js
@@ -38,7 +38,7 @@ const app = express();
 //     credential: true,
 // };
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "https://x-liart.vercel.app" }));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
