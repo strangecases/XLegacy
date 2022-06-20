a3bd7f9595ba33766dc3eca13472f953a6d505a3
Bavana Venkata Sumanth <sumanthbv8@gmial.com>
2022-06-20 20:24:57 +0530

    final-push-all-4

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,19 @@ const corsOptions = {
     },
 };
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    next();
+});
+
 app.use(cors(corsOptions));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
